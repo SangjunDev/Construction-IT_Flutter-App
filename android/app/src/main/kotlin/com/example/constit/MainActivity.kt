b9929397e076caf9c513b2b4f3dc75859364a4b3
Sang Jun Kim <sangjunkim@Sangui-MacBookPro.local>
2023-01-05 11:05:32 +0900

    frist commit

---
+++ android/app/src/main/kotlin/com/example/constit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.constit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: WebView(
+          initialUrl: 'https://cndi-rs.co.kr',
+          javascriptMode: JavascriptMode.unrestricted,
+          gestureNavigationEnabled: true,
+          userAgent: "random",
+        ),
+      ),
+    );
+  }
+}
